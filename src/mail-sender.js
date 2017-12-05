72b02a8743e0960b15b339766afb2cf27c00b83e
Jonathan Chiocchio <jchiocchio@gmail.com>
2017-12-05 11:19:56 -0300

    Changed the Mail Sender component to use ES6 class syntax

--- src/mail-sender.js
+++ src/mail-sender.js
@@ -5,56 +5,56 @@ const mailTransporterCreator = require('./mail-transporter-creator');
 const MAX_ATTACHMENT_SIZE_IN_MB = 1;
 const MAX_ATTACHMENT_SIZE_IN_BYTES = MAX_ATTACHMENT_SIZE_IN_MB * 1024 * 1024;
 
-// TODO Convert to ES6 class style
-// Constructor
-function MailSender(config) {
-	this.config = config;
-	this.transporter = mailTransporterCreator.createTransporter(config);
-}
+class MailSender {
+	constructor(config) {
+		this.config = config;
+		this.transporter = mailTransporterCreator.createTransporter(config);
+	}
 
-MailSender.prototype.sendEmail = function sendEmail(options) {
-	const self = this;
-	return new Promise((resolve, reject) => {
-		if (options.attachments) {
-			self._validateAttachments(options.attachments, reject);
-		}
-
-		let mailOptions = {
-			from: options.from || self.config.user,
-			to: options.to,
-			subject: options.subject,
-			text: options.text,
-			html: options.html,
-			attachments: options.attachments
-		};
-		// TODO Use Node's util.promisify
-		self.transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				// TODO try to determine the nature of the failure better, in order to report it more accurately
-				return reject({success: false, status: 503, message: error.message});
+	sendEmail(options) {
+		const self = this;
+		return new Promise((resolve, reject) => {
+			if (options.attachments) {
+				MailSender._validateAttachments(options.attachments, reject);
 			}
-			return resolve({success: true, info: info});
-		});
-	});
-};
 
-MailSender.prototype._validateAttachments = function _validateAttachments(attachments, reject) {
-	for (let attachment of attachments) {
-		if (!attachment.contentLength) {
-			return reject({
-				success: false,
-				status: 400,
-				message: `The property "attachment.contentLength" is required.`
+			let mailOptions = {
+				from: options.from || self.config.user,
+				to: options.to,
+				subject: options.subject,
+				text: options.text,
+				html: options.html,
+				attachments: options.attachments
+			};
+			// TODO Use Node 8's util.promisify
+			self.transporter.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					// TODO try to determine the nature of the failure better, in order to report it more accurately
+					return reject({success: false, status: 503, message: error.message});
+				}
+				return resolve({success: true, info: info});
 			});
-		}
-		if (attachment.contentLength >= MAX_ATTACHMENT_SIZE_IN_BYTES) {
-			return reject({
-				success: false,
-				status: 400,
-				message: `The attachment is too big. Max. file size: ${MAX_ATTACHMENT_SIZE_IN_MB} MB`
-			});
+		});
+	}
+
+	static _validateAttachments(attachments, reject) {
+		for (let attachment of attachments) {
+			if (!attachment.contentLength) {
+				return reject({
+					success: false,
+					status: 400,
+					message: `The property "attachment.contentLength" is required.`
+				});
+			}
+			if (attachment.contentLength >= MAX_ATTACHMENT_SIZE_IN_BYTES) {
+				return reject({
+					success: false,
+					status: 400,
+					message: `The attachment is too big. Max. file size: ${MAX_ATTACHMENT_SIZE_IN_MB} MB`
+				});
+			}
 		}
 	}
-};
+}
 
 module.exports = MailSender;
