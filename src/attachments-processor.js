c4521d65200f4372c313c8a13abae6140061a964
Jonathan Chiocchio <jchiocchio@gmail.com>
2017-12-12 09:34:45 -0300

    extracted the attachments processing logic to a separate component.

---
+++ src/attachments-processor.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const _ = require('lodash');
+
+const BYTES_IN_A_MEGABYTE = 1024 * 1024;
+const ATTACHMENTS_FOOTER_PREFIX = "Please see the following link(s):";
+
+class AttachmentsProcessor {
+	constructor(config) {
+		this.config = config;
+	}
+
+	async processAttachments(options) {
+		const self = this;
+		return new Promise((resolve, reject) => {
+
+			const result = _.pick(options, 'text', 'html');
+			result.attachments = [];
+			const linksToOversizedAttachments = [];
+
+			const attachments = options.attachments || [];
+
+			if (!(attachments instanceof Array) || attachments.length > self.config.maxAttachments) {
+				return reject({
+					success: false,
+					status: 400,
+					message: `The "attachments" option (when present) has to be an array with no more than ${self.config.maxAttachments} attachment(s).`
+				});
+			}
+			for (let attachment of options.attachments) {
+				const contentLength = attachment.contentLength;
+
+				if (!contentLength || !Number.isInteger(contentLength) || contentLength <= 0) {
+					return reject({
+						success: false,
+						status: 400,
+						message: `The option "attachment.contentLength" is required and has to be a positive integer.`
+					});
+				}
+				const maxAttachmentSize = self.config.maxAttachmentSizeInMb * BYTES_IN_A_MEGABYTE;
+
+				if (attachment.contentLength > maxAttachmentSize) {
+					linksToOversizedAttachments.push(attachment.href);
+				} else {
+					result.attachments.push(attachment);
+				}
+			}
+			if (linksToOversizedAttachments.length > 0) {
+				if (result.text) {
+					result.text += "\n\n" + ATTACHMENTS_FOOTER_PREFIX + "\n" + _.join(linksToOversizedAttachments, '\n');
+				}
+				if (result.html) {
+					result.html += "<p>" + ATTACHMENTS_FOOTER_PREFIX + "<br>" + _.join(linksToOversizedAttachments, '<br>');
+				}
+			}
+			return resolve(result);
+		});
+	}
+}
+
+module.exports = AttachmentsProcessor;
--- src/mail-sender.js
+++ src/mail-sender.js
@@ -2,14 +2,13 @@
 
 const _ = require('lodash');
 const mailTransporterCreator = require('./mail-transporter-creator');
-
-const BYTES_IN_A_MEGABYTE = 1024 * 1024;
-const ATTACHMENTS_FOOTER_PREFIX = "Please see the following link(s):";
+const AttachmentsProcessor = require('./attachments-processor');
 
 class MailSender {
 	constructor(config) {
 		this.config = config;
 		this.transporter = mailTransporterCreator.createTransporter(config);
+		this.attachmentsProcessor = new AttachmentsProcessor(config);
 	}
 
 	sendEmail(options) {
@@ -17,7 +16,7 @@ class MailSender {
 		return new Promise(async (resolve, reject) => {
 			let postProcessedOptions;
 			try {
-				postProcessedOptions = await self._processAttachments(options);
+				postProcessedOptions = await self.attachmentsProcessor.processAttachments(options);
 			} catch (err) {
 				return reject(err);
 			}
@@ -37,52 +36,6 @@ class MailSender {
 				}
 				return resolve({success: true, info: info});
 			});
-		});
-	}
-
-	_processAttachments(options) {
-		return new Promise((resolve, reject) => {
-
-			const result = _.pick(options, 'text', 'html');
-			result.attachments = [];
-			const linksToOversizedAttachments = [];
-
-			const attachments = options.attachments || [];
-
-			if (!(attachments instanceof Array) || attachments.length > this.config.maxAttachments) {
-				return reject({
-					success: false,
-					status: 400,
-					message: `The "attachments" option (when present) has to be an array with no more than ${this.config.maxAttachments} attachment(s).`
-				});
-			}
-			for (let attachment of options.attachments) {
-				const contentLength = attachment.contentLength;
-
-				if (!contentLength || !Number.isInteger(contentLength) || contentLength <= 0) {
-					return reject({
-						success: false,
-						status: 400,
-						message: `The option "attachment.contentLength" is required and has to be a positive integer.`
-					});
-				}
-				const maxAttachmentSize = this.config.maxAttachmentSizeInMb * BYTES_IN_A_MEGABYTE;
-
-				if (attachment.contentLength > maxAttachmentSize) {
-					linksToOversizedAttachments.push(attachment.href);
-				} else {
-					result.attachments.push(attachment);
-				}
-			}
-			if (linksToOversizedAttachments.length > 0) {
-				if (result.text) {
-					result.text += "\n\n" + ATTACHMENTS_FOOTER_PREFIX + "\n" + _.join(linksToOversizedAttachments, '\n');
-				}
-				if (result.html) {
-					result.html += "<p>" + ATTACHMENTS_FOOTER_PREFIX + "<br>" + _.join(linksToOversizedAttachments, '<br>')
-				}
-			}
-			return resolve(result);
 		});
 	}
 }
